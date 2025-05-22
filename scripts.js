cbddce2ba6c219705daf9967dcc45dd05d2b879b
catlicker <211680666+catlicky@users.noreply.github.com>
2025-05-22 15:57:19 -0700

    improvments

--- scripts.js
+++ scripts.js
@@ -4,6 +4,14 @@ const video = document.getElementById('bgVideo');
 
 overlay.addEventListener('click', () => {
     overlay.classList.add('hidden');
+    
     music.play();
+    music.volume = 0.5;
+    
     video.play();
+
+    const card = document.getElementById("card");
+    
+    card.hidden = false;
+    card.classList.add("fadeClass");
 });
